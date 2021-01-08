a8ab02f064fb81b40d96279f02b2fe13e99b2653
alisonhussey <alisonhussey@gmail.com>
2021-01-08 02:24:51 +0000

    update addProperty using sql

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -112,14 +112,13 @@ exports.getAllReservations = getAllReservations;
 // LIMIT 10;
 
 const getAllProperties = function(options, limit = 10) {
-  console.log(options)
   // 1
   const queryParams = [];
   // 2
   let queryString = `
   SELECT properties.*, avg(property_reviews.rating) as average_rating
   FROM properties
-  JOIN property_reviews ON properties.id = property_id
+  LEFT JOIN property_reviews ON properties.id = property_id
   `;
 
   // 3
@@ -187,12 +186,35 @@ exports.getAllProperties = getAllProperties;
  * @param {{}} property An object containing all of the property details.
  * @return {Promise<{}>} A promise to the property.
  */
+
+
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
-}
+ const queryString = `
+ INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code)
+ VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+ RETURNING *;
+ `
+
+const values = [
+  property.owner_id,
+  property.title,
+  property.description,
+  property.thumbnail_photo_url,
+  property.cover_photo_url,
+  property.cost_per_night,
+  property.parking_spaces,
+  property.number_of_bathrooms,
+  property.number_of_bedrooms,
+  property.country,
+  property.street,
+  property.city,
+  property.province,
+  property.post_code
+]
+return pool.query(queryString, values)
+  .then(res => res.rows[0])
+  .catch(err => console.log(err.stack));
+};
 exports.addProperty = addProperty;
 
-//i'll have to change the join to make sure that properties are there eve if property_reviews is null.
+//i'll have to change the join in getallproperties to make sure that properties are there eve if property_reviews is null.
